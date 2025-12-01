88418fa222c3fa0b2ee5b9b08bc0c4a596d91cb9
Elmanda1 <falihelmanda@gmail.com>
2025-12-01 18:25:48 +0700

    Feat: Enhance ChangePassword form with improved UI, validation, and logo loading functionality

--- vb_app/ProjectPBL/ChangePassword.Designer.vb
+++ vb_app/ProjectPBL/ChangePassword.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ChangePassword
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,119 +20,199 @@ Partial Class ChangePassword
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        PictureBox2 = New PictureBox()
         PictureBox1 = New PictureBox()
-        Label2 = New Label()
-        TextBox1 = New TextBox()
-        Label1 = New Label()
-        TextBox2 = New TextBox()
-        Label3 = New Label()
+        Panel2 = New Panel()
+        Button2 = New Button()
+        Button1 = New Button()
         TextBox3 = New TextBox()
         Label4 = New Label()
-        Button1 = New Button()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        Panel1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(41), CByte(128), CByte(185))
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(900, 180)
+        Panel1.TabIndex = 0
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Location = New Point(695, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(180, 155)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Image = ProjectPBL.Resources.logo
-        PictureBox1.Location = New Point(256, 34)
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Location = New Point(28, 12)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(259, 205)
-        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
-        PictureBox1.TabIndex = 9
+        PictureBox1.Size = New Size(180, 155)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
         PictureBox1.TabStop = False
         ' 
-        ' Label2
+        ' Panel2
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(137, 344)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(107, 20)
-        Label2.TabIndex = 22
-        Label2.Text = "Password Baru:"
+        Panel2.BackColor = Color.WhiteSmoke
+        Panel2.Controls.Add(Button2)
+        Panel2.Controls.Add(Button1)
+        Panel2.Controls.Add(TextBox3)
+        Panel2.Controls.Add(Label4)
+        Panel2.Controls.Add(TextBox2)
+        Panel2.Controls.Add(Label3)
+        Panel2.Controls.Add(Label1)
+        Panel2.Controls.Add(TextBox1)
+        Panel2.Controls.Add(Label2)
+        Panel2.Dock = DockStyle.Fill
+        Panel2.Location = New Point(0, 180)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(900, 470)
+        Panel2.TabIndex = 1
         ' 
-        ' TextBox1
+        ' Button2
         ' 
-        TextBox1.Location = New Point(259, 341)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(256, 27)
-        TextBox1.TabIndex = 23
+        Button2.BackColor = Color.FromArgb(CByte(149), CByte(165), CByte(166))
+        Button2.Cursor = Cursors.Hand
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
+        Button2.ForeColor = Color.White
+        Button2.Location = New Point(280, 390)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(340, 50)
+        Button2.TabIndex = 8
+        Button2.Text = "? Kembali ke Login"
+        Button2.UseVisualStyleBackColor = False
         ' 
-        ' Label1
+        ' Button1
         ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI", 13F)
-        Label1.Location = New Point(256, 253)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(273, 30)
-        Label1.TabIndex = 24
-        Label1.Text = "Ganti Password Akun anda!"
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Location = New Point(259, 298)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(256, 27)
-        TextBox2.TabIndex = 26
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(166, 301)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(78, 20)
-        Label3.TabIndex = 25
-        Label3.Text = "Username:"
+        Button1.BackColor = Color.FromArgb(CByte(230), CByte(126), CByte(34))
+        Button1.Cursor = Cursors.Hand
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold)
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(280, 330)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(340, 50)
+        Button1.TabIndex = 7
+        Button1.Text = "?? Ganti Password"
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(259, 386)
+        TextBox3.Font = New Font("Segoe UI", 12.0F)
+        TextBox3.Location = New Point(280, 280)
         TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(256, 27)
-        TextBox3.TabIndex = 28
+        TextBox3.Size = New Size(340, 34)
+        TextBox3.TabIndex = 6
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(74, 393)
+        Label4.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
+        Label4.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
+        Label4.Location = New Point(280, 250)
         Label4.Name = "Label4"
-        Label4.Size = New Size(170, 20)
-        Label4.TabIndex = 27
-        Label4.Text = "Verifikasi Password Baru:"
+        Label4.Size = New Size(211, 25)
+        Label4.TabIndex = 5
+        Label4.Text = "Konfirmasi Password Baru"
         ' 
-        ' Button1
+        ' TextBox2
         ' 
-        Button1.Location = New Point(400, 435)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(115, 62)
-        Button1.TabIndex = 29
-        Button1.Text = "Ganti Password"
-        Button1.UseVisualStyleBackColor = True
+        TextBox2.Font = New Font("Segoe UI", 12.0F)
+        TextBox2.Location = New Point(280, 200)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(340, 34)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
+        Label3.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
+        Label3.Location = New Point(280, 170)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(133, 25)
+        Label3.TabIndex = 3
+        Label3.Text = "Password Baru"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        Label1.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
+        Label1.Location = New Point(310, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(280, 41)
+        Label1.TabIndex = 2
+        Label1.Text = "Reset Password ??"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 12.0F)
+        TextBox1.Location = New Point(280, 120)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(340, 34)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
+        Label2.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
+        Label2.Location = New Point(280, 90)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(99, 25)
+        Label2.TabIndex = 0
+        Label2.Text = "Username"
         ' 
         ' ChangePassword
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 509)
-        Controls.Add(Button1)
-        Controls.Add(TextBox3)
-        Controls.Add(Label4)
-        Controls.Add(TextBox2)
-        Controls.Add(Label3)
-        Controls.Add(Label1)
-        Controls.Add(TextBox1)
-        Controls.Add(Label2)
-        Controls.Add(PictureBox1)
+        ClientSize = New Size(900, 650)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
         Name = "ChangePassword"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "Change Password - SITOPSI"
+        Panel1.ResumeLayout(False)
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
 
+    Friend WithEvents Panel1 As Panel
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel2 As Panel
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
@@ -141,4 +221,5 @@ Partial Class ChangePassword
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- vb_app/ProjectPBL/ChangePassword.vb
+++ vb_app/ProjectPBL/ChangePassword.vb
@@ -1,26 +1,61 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class ChangePassword
+    ' Reference ke parent login form
+    Public Property ParentLogin As LoginForm
+
     Private Sub ChangePassword_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Set password character
-        TextBox1.PasswordChar = "●"c
+        TextBox2.PasswordChar = "●"c
         TextBox3.PasswordChar = "●"c
 
         ' Center form
         Me.StartPosition = FormStartPosition.CenterScreen
+
+        ' Load logo SITOPSI kiri
+        Try
+            Dim logoPath As String = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logobg.jpg")
+            If System.IO.File.Exists(logoPath) Then
+                PictureBox1.Image = Image.FromFile(logoPath)
+            Else
+                ' Try PNG version
+                logoPath = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logo.png")
+                If System.IO.File.Exists(logoPath) Then
+                    PictureBox1.Image = Image.FromFile(logoPath)
+                End If
+            End If
+        Catch ex As Exception
+            ' Silent fail - logo tidak ditemukan
+        End Try
+
+        ' Load logo PNJ kanan
+        Try
+            Dim pnjPath As String = System.IO.Path.Combine(Application.StartupPath, "..\..\..\Logo_Politeknik_Negeri_Jakarta.jpg")
+            If System.IO.File.Exists(pnjPath) Then
+                PictureBox2.Image = Image.FromFile(pnjPath)
+            Else
+                ' Try PNG version
+                pnjPath = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logo_pnj.png")
+                If System.IO.File.Exists(pnjPath) Then
+                    PictureBox2.Image = Image.FromFile(pnjPath)
+                End If
+            End If
+        Catch ex As Exception
+            ' Silent fail - logo tidak ditemukan
+        End Try
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ' Validasi input
-        If String.IsNullOrWhiteSpace(TextBox2.Text) Then
+        If String.IsNullOrWhiteSpace(TextBox1.Text) Then
             MessageBox.Show("Username tidak boleh kosong!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            TextBox2.Focus()
+            TextBox1.Focus()
             Return
         End If
 
-        If String.IsNullOrWhiteSpace(TextBox1.Text) Then
+        If String.IsNullOrWhiteSpace(TextBox2.Text) Then
             MessageBox.Show("Password baru tidak boleh kosong!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            TextBox1.Focus()
+            TextBox2.Focus()
             Return
         End If
 
@@ -30,69 +65,73 @@ Public Class ChangePassword
             Return
         End If
 
-        ' Validasi password minimal 5 karakter
-        If TextBox1.Text.Length < 5 Then
+        ' Validasi panjang password minimal 5 karakter
+        If TextBox2.Text.Length < 5 Then
             MessageBox.Show("Password minimal 5 karakter!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            TextBox1.Focus()
+            TextBox2.Focus()
             Return
         End If
 
         ' Validasi password cocok
-        If TextBox1.Text <> TextBox3.Text Then
-            MessageBox.Show("Password baru dan verifikasi password tidak cocok!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        If TextBox2.Text <> TextBox3.Text Then
+            MessageBox.Show("Password tidak cocok!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             TextBox3.Clear()
             TextBox3.Focus()
             Return
         End If
 
-        ' Proses ganti password
         Try
             Using conn As New MySqlConnection(ConnectionString)
                 conn.Open()
 
                 ' Cek apakah username ada
-                Dim checkQuery As String = "SELECT COUNT(*) FROM users WHERE username = @username"
-                Using checkCmd As New MySqlCommand(checkQuery, conn)
-                    checkCmd.Parameters.AddWithValue("@username", TextBox2.Text.Trim())
-                    Dim count As Integer = Convert.ToInt32(checkCmd.ExecuteScalar())
+                Dim checkQuery As String = "SELECT id, fullname FROM users WHERE username = @username"
+                Dim userId As Integer = 0
+                Dim fullname As String = ""
 
-                    If count = 0 Then
-                        MessageBox.Show("Username tidak ditemukan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                        Return
-                    End If
+                Using checkCmd As New MySqlCommand(checkQuery, conn)
+                    checkCmd.Parameters.AddWithValue("@username", TextBox1.Text.Trim())
+
+                    Using reader As MySqlDataReader = checkCmd.ExecuteReader()
+                        If reader.Read() Then
+                            userId = Convert.ToInt32(reader("id"))
+                            fullname = reader("fullname").ToString()
+                        Else
+                            MessageBox.Show("Username tidak ditemukan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            Return
+                        End If
+                    End Using
                 End Using
 
                 ' Update password
-                Dim updateQuery As String = "UPDATE users SET password = @password, updated_at = NOW() WHERE username = @username"
+                Dim updateQuery As String = "UPDATE users SET password = @password, updated_at = NOW() WHERE id = @userId"
                 Using updateCmd As New MySqlCommand(updateQuery, conn)
-                    updateCmd.Parameters.AddWithValue("@password", TextBox1.Text)
-                    updateCmd.Parameters.AddWithValue("@username", TextBox2.Text.Trim())
+                    updateCmd.Parameters.AddWithValue("@password", TextBox2.Text)
+                    updateCmd.Parameters.AddWithValue("@userId", userId)
 
                     Dim result As Integer = updateCmd.ExecuteNonQuery()
 
                     If result > 0 Then
                         ' Log activity
                         Try
-                            Dim logQuery As String = "INSERT INTO activity_logs (user_id, aksi, detail, waktu) " &
-                                                    "VALUES ((SELECT id FROM users WHERE username = @username), @aksi, @detail, NOW())"
+                            Dim logQuery As String = "INSERT INTO activity_logs (user_id, aksi, detail, waktu) VALUES (@userId, 'Change Password', @detail, NOW())"
                             Using logCmd As New MySqlCommand(logQuery, conn)
-                                logCmd.Parameters.AddWithValue("@username", TextBox2.Text.Trim())
-                                logCmd.Parameters.AddWithValue("@aksi", "Change Password")
-                                logCmd.Parameters.AddWithValue("@detail", $"User {TextBox2.Text.Trim()} mengganti password")
+                                logCmd.Parameters.AddWithValue("@userId", userId)
+                                logCmd.Parameters.AddWithValue("@detail", $"User {TextBox1.Text} berhasil mengganti password")
                                 logCmd.ExecuteNonQuery()
                             End Using
                         Catch
-                            ' Silent fail untuk log
+                            ' Silent fail for activity log
                         End Try
 
-                        MessageBox.Show("Password berhasil diganti! Silakan login dengan password baru.", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        MessageBox.Show($"Password berhasil diubah untuk {fullname}!" & vbCrLf & "Silakan login dengan password baru.", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
                         ' Kembali ke login form
                         Dim loginForm As New LoginForm()
                         loginForm.Show()
                         Me.Close()
                     Else
-                        MessageBox.Show("Gagal mengganti password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        MessageBox.Show("Gagal mengubah password! Silakan coba lagi.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
                     End If
                 End Using
             End Using
@@ -104,17 +143,24 @@ Public Class ChangePassword
         End Try
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-        ' Keep existing
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Kembali ke login form
+        If ParentLogin IsNot Nothing Then
+            ParentLogin.Show()
+        Else
+            Dim loginForm As New LoginForm()
+            loginForm.Show()
+        End If
+        Me.Close()
     End Sub
 
     Private Sub ChangePassword_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         ' Kembali ke login form
-        Dim loginForm As New LoginForm()
-        loginForm.Show()
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
+        If ParentLogin IsNot Nothing Then
+            ParentLogin.Show()
+        Else
+            Dim loginForm As New LoginForm()
+            loginForm.Show()
+        End If
     End Sub
 End Class
