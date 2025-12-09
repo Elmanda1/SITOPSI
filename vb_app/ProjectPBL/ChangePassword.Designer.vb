463631f8e67e75d3ec9c31bffce4048826d73cf7
Elmanda1 <falihelmanda@gmail.com>
2025-12-09 10:18:39 +0700

    Feat: Revamp ChangePassword form with improved UI, added hover effects, and updated database queries

--- vb_app/ProjectPBL/ChangePassword.Designer.vb
+++ vb_app/ProjectPBL/ChangePassword.Designer.vb
@@ -22,204 +22,204 @@ Partial Class ChangePassword
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Panel1 = New Panel()
-        PictureBox2 = New PictureBox()
-        PictureBox1 = New PictureBox()
-        Panel2 = New Panel()
-        Button2 = New Button()
-        Button1 = New Button()
-        TextBox3 = New TextBox()
-        Label4 = New Label()
-        TextBox2 = New TextBox()
-        Label3 = New Label()
-        Label1 = New Label()
+        Dim navyColor As Color = Color.FromArgb(12, 45, 72)
+        
+        PictureBoxLogo = New PictureBox()
+        cardPanel = New Panel()
+        Label1 = New Label() ' Title
+        Label2 = New Label() ' Username
         TextBox1 = New TextBox()
-        Label2 = New Label()
-        Panel1.SuspendLayout()
-        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
-        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
-        Panel2.SuspendLayout()
+        Label3 = New Label() ' New Password
+        TextBox2 = New TextBox()
+        Label4 = New Label() ' Confirm Password
+        TextBox3 = New TextBox()
+        Button1 = New Button() ' Save
+        Button2 = New Button() ' Back
+
+        cardPanel.SuspendLayout()
+        CType(PictureBoxLogo, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
-        ' 
-        ' Panel1
-        ' 
-        Panel1.BackColor = Color.FromArgb(CByte(41), CByte(128), CByte(185))
-        Panel1.Controls.Add(PictureBox2)
-        Panel1.Controls.Add(PictureBox1)
-        Panel1.Dock = DockStyle.Top
-        Panel1.Location = New Point(0, 0)
-        Panel1.Name = "Panel1"
-        Panel1.Size = New Size(900, 180)
-        Panel1.TabIndex = 0
-        ' 
-        ' PictureBox2
-        ' 
-        PictureBox2.BackColor = Color.Transparent
-        PictureBox2.Location = New Point(695, 12)
-        PictureBox2.Name = "PictureBox2"
-        PictureBox2.Size = New Size(180, 155)
-        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
-        PictureBox2.TabIndex = 1
-        PictureBox2.TabStop = False
-        ' 
-        ' PictureBox1
-        ' 
-        PictureBox1.BackColor = Color.Transparent
-        PictureBox1.Location = New Point(28, 12)
-        PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(180, 155)
-        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
-        PictureBox1.TabIndex = 0
-        PictureBox1.TabStop = False
-        ' 
-        ' Panel2
-        ' 
-        Panel2.BackColor = Color.WhiteSmoke
-        Panel2.Controls.Add(Button2)
-        Panel2.Controls.Add(Button1)
-        Panel2.Controls.Add(TextBox3)
-        Panel2.Controls.Add(Label4)
-        Panel2.Controls.Add(TextBox2)
-        Panel2.Controls.Add(Label3)
-        Panel2.Controls.Add(Label1)
-        Panel2.Controls.Add(TextBox1)
-        Panel2.Controls.Add(Label2)
-        Panel2.Dock = DockStyle.Fill
-        Panel2.Location = New Point(0, 180)
-        Panel2.Name = "Panel2"
-        Panel2.Size = New Size(900, 470)
-        Panel2.TabIndex = 1
-        ' 
-        ' Button2
-        ' 
-        Button2.BackColor = Color.FromArgb(CByte(149), CByte(165), CByte(166))
-        Button2.Cursor = Cursors.Hand
-        Button2.FlatAppearance.BorderSize = 0
-        Button2.FlatStyle = FlatStyle.Flat
-        Button2.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
-        Button2.ForeColor = Color.White
-        Button2.Location = New Point(280, 390)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(340, 50)
-        Button2.TabIndex = 8
-        Button2.Text = "? Kembali ke Login"
-        Button2.UseVisualStyleBackColor = False
-        ' 
-        ' Button1
-        ' 
-        Button1.BackColor = Color.FromArgb(CByte(230), CByte(126), CByte(34))
-        Button1.Cursor = Cursors.Hand
-        Button1.FlatAppearance.BorderSize = 0
-        Button1.FlatStyle = FlatStyle.Flat
-        Button1.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold)
-        Button1.ForeColor = Color.White
-        Button1.Location = New Point(280, 330)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(340, 50)
-        Button1.TabIndex = 7
-        Button1.Text = "?? Ganti Password"
-        Button1.UseVisualStyleBackColor = False
-        ' 
-        ' TextBox3
-        ' 
-        TextBox3.Font = New Font("Segoe UI", 12.0F)
-        TextBox3.Location = New Point(280, 280)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(340, 34)
-        TextBox3.TabIndex = 6
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
-        Label4.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
-        Label4.Location = New Point(280, 250)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(211, 25)
-        Label4.TabIndex = 5
-        Label4.Text = "Konfirmasi Password Baru"
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Font = New Font("Segoe UI", 12.0F)
-        TextBox2.Location = New Point(280, 200)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(340, 34)
-        TextBox2.TabIndex = 4
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
-        Label3.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
-        Label3.Location = New Point(280, 170)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(133, 25)
-        Label3.TabIndex = 3
-        Label3.Text = "Password Baru"
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
-        Label1.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
-        Label1.Location = New Point(310, 30)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(280, 41)
-        Label1.TabIndex = 2
-        Label1.Text = "Reset Password ??"
-        Label1.TextAlign = ContentAlignment.MiddleCenter
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.Font = New Font("Segoe UI", 12.0F)
-        TextBox1.Location = New Point(280, 120)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(340, 34)
-        TextBox1.TabIndex = 1
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Segoe UI", 11.0F, FontStyle.Bold)
-        Label2.ForeColor = Color.FromArgb(CByte(52), CByte(73), CByte(94))
-        Label2.Location = New Point(280, 90)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(99, 25)
-        Label2.TabIndex = 0
-        Label2.Text = "Username"
+
         ' 
         ' ChangePassword
         ' 
         AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(900, 650)
-        Controls.Add(Panel2)
-        Controls.Add(Panel1)
+        BackColor = Color.FromArgb(240, 242, 245)
+        ClientSize = New Size(600, 650)
+        Controls.Add(PictureBoxLogo)
+        Controls.Add(cardPanel)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "ChangePassword"
         StartPosition = FormStartPosition.CenterScreen
-        Text = "Change Password - SITOPSI"
-        Panel1.ResumeLayout(False)
-        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
-        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
-        Panel2.ResumeLayout(False)
-        Panel2.PerformLayout()
+        Text = "SITOPSI - Ganti Password"
+
+        ' 
+        ' PictureBoxLogo
+        ' 
+        PictureBoxLogo.BackColor = Color.Transparent
+        PictureBoxLogo.Image = System.Drawing.Image.FromFile("logo.png")
+        PictureBoxLogo.Location = New Point((Me.ClientSize.Width - 100) \ 2, 20)
+        PictureBoxLogo.Name = "PictureBoxLogo"
+        PictureBoxLogo.Size = New Size(100, 80)
+        PictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBoxLogo.TabIndex = 0
+        PictureBoxLogo.TabStop = False
+
+        ' 
+        ' cardPanel
+        ' 
+        cardPanel.BackColor = Color.White
+        cardPanel.BorderStyle = BorderStyle.FixedSingle
+        cardPanel.Location = New Point(75, 120)
+        cardPanel.Size = New Size(450, 480)
+        cardPanel.Controls.Add(Label1)
+        cardPanel.Controls.Add(Label2)
+        cardPanel.Controls.Add(TextBox1)
+        cardPanel.Controls.Add(Label3)
+        cardPanel.Controls.Add(TextBox2)
+        cardPanel.Controls.Add(Label4)
+        cardPanel.Controls.Add(TextBox3)
+        cardPanel.Controls.Add(Button1)
+        cardPanel.Controls.Add(Button2)
+
+        ' 
+        ' Label1 (Title)
+        ' 
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI", 18.0F, FontStyle.Bold)
+        Label1.ForeColor = navyColor
+        Label1.Location = New Point(0, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(450, 41)
+        Label1.TabIndex = 2
+        Label1.Text = "Reset Password"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        
+        ' 
+        ' Label2 (Username)
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        Label2.ForeColor = navyColor
+        Label2.Location = New Point(71, 80)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 23)
+        Label2.TabIndex = 0
+        Label2.Text = "Username"
+
+        ' 
+        ' TextBox1 (Username)
+        ' 
+        TextBox1.BackColor = Color.White
+        TextBox1.ForeColor = Color.Black
+        TextBox1.Font = New Font("Segoe UI", 10.0F)
+        TextBox1.Location = New Point(75, 106)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(300, 40)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = ""
+
+        ' 
+        ' Label3 (New Password)
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        Label3.ForeColor = navyColor
+        Label3.Location = New Point(71, 160)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(127, 23)
+        Label3.TabIndex = 3
+        Label3.Text = "Password Baru"
+
+        ' 
+        ' TextBox2 (New Password)
+        ' 
+        TextBox2.BackColor = Color.White
+        TextBox2.ForeColor = Color.Black
+        TextBox2.Font = New Font("Segoe UI", 10.0F)
+        TextBox2.Location = New Point(75, 186)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(300, 40)
+        TextBox2.TabIndex = 4
+        TextBox2.PasswordChar = "●"c
+        TextBox2.Text = ""
+        
+        ' 
+        ' Label4 (Confirm Password)
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        Label4.ForeColor = navyColor
+        Label4.Location = New Point(71, 240)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(201, 23)
+        Label4.TabIndex = 5
+        Label4.Text = "Konfirmasi Password Baru"
+
+        ' 
+        ' TextBox3 (Confirm Password)
+        ' 
+        TextBox3.BackColor = Color.White
+        TextBox3.ForeColor = Color.Black
+        TextBox3.Font = New Font("Segoe UI", 10.0F)
+        TextBox3.Location = New Point(75, 266)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(300, 40)
+        TextBox3.TabIndex = 6
+        TextBox3.PasswordChar = "●"c
+        TextBox3.Text = ""
+
+        ' 
+        ' Button1 (Save)
+        ' 
+        Button1.BackColor = navyColor
+        Button1.Cursor = Cursors.Hand
+        Button1.Font = New Font("Segoe UI", 12.0F, FontStyle.Bold)
+        Button1.ForeColor = Color.White
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.Location = New Point(75, 350)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(300, 50)
+        Button1.TabIndex = 7
+        Button1.Text = "Ganti Password"
+
+        ' 
+        ' Button2 (Back)
+        ' 
+        Button2.BackColor = Color.White
+        Button2.Cursor = Cursors.Hand
+        Button2.Font = New Font("Segoe UI", 10.0F, FontStyle.Bold)
+        Button2.ForeColor = navyColor
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.FlatAppearance.BorderSize = 1
+        Button2.FlatAppearance.BorderColor = navyColor
+        Button2.Location = New Point(75, 410)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(300, 50)
+        Button2.TabIndex = 8
+        Button2.Text = "⬅ Kembali ke Login"
+        
+        cardPanel.ResumeLayout(False)
+        cardPanel.PerformLayout()
+        CType(PictureBoxLogo, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents Panel2 As Panel
+    Friend WithEvents PictureBoxLogo As PictureBox
+    Friend WithEvents cardPanel As Panel
+    Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
 End Class
--- vb_app/ProjectPBL/ChangePassword.vb
+++ vb_app/ProjectPBL/ChangePassword.vb
@@ -1,48 +1,24 @@
-﻿Imports MySql.Data.MySqlClient
+Imports MySql.Data.MySqlClient
 
 Public Class ChangePassword
-    ' Reference ke parent login form
     Public Property ParentLogin As LoginForm
+    Private isSuccess As Boolean = False
+
+    ' --- Colors ---
+    Private ReadOnly navyColor As Color = Color.FromArgb(12, 45, 72)
+    Private ReadOnly lightNavyColor As Color = Color.FromArgb(28, 68, 105)
+    Private ReadOnly whiteColor As Color = Color.White
+    Private ReadOnly lightGreyHoverColor As Color = Color.FromArgb(236, 240, 241)
 
     Private Sub ChangePassword_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Set password character
-        TextBox2.PasswordChar = "●"c
-        TextBox3.PasswordChar = "●"c
-
         ' Center form
         Me.StartPosition = FormStartPosition.CenterScreen
 
-        ' Load logo SITOPSI kiri
-        Try
-            Dim logoPath As String = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logobg.jpg")
-            If System.IO.File.Exists(logoPath) Then
-                PictureBox1.Image = Image.FromFile(logoPath)
-            Else
-                ' Try PNG version
-                logoPath = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logo.png")
-                If System.IO.File.Exists(logoPath) Then
-                    PictureBox1.Image = Image.FromFile(logoPath)
-                End If
-            End If
-        Catch ex As Exception
-            ' Silent fail - logo tidak ditemukan
-        End Try
-
-        ' Load logo PNJ kanan
-        Try
-            Dim pnjPath As String = System.IO.Path.Combine(Application.StartupPath, "..\..\..\Logo_Politeknik_Negeri_Jakarta.jpg")
-            If System.IO.File.Exists(pnjPath) Then
-                PictureBox2.Image = Image.FromFile(pnjPath)
-            Else
-                ' Try PNG version
-                pnjPath = System.IO.Path.Combine(Application.StartupPath, "..\..\..\logo_pnj.png")
-                If System.IO.File.Exists(pnjPath) Then
-                    PictureBox2.Image = Image.FromFile(pnjPath)
-                End If
-            End If
-        Catch ex As Exception
-            ' Silent fail - logo tidak ditemukan
-        End Try
+        ' Add hover effects
+        AddHandler Button1.MouseEnter, AddressOf Button1_MouseEnter
+        AddHandler Button1.MouseLeave, AddressOf Button1_MouseLeave
+        AddHandler Button2.MouseEnter, AddressOf Button2_MouseEnter
+        AddHandler Button2.MouseLeave, AddressOf Button2_MouseLeave
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -85,7 +61,7 @@ Public Class ChangePassword
                 conn.Open()
 
                 ' Cek apakah username ada
-                Dim checkQuery As String = "SELECT id, fullname FROM users WHERE username = @username"
+                Dim checkQuery As String = "SELECT user_id, fullname FROM users WHERE username = @username"
                 Dim userId As Integer = 0
                 Dim fullname As String = ""
 
@@ -94,7 +70,7 @@ Public Class ChangePassword
 
                     Using reader As MySqlDataReader = checkCmd.ExecuteReader()
                         If reader.Read() Then
-                            userId = Convert.ToInt32(reader("id"))
+                            userId = Convert.ToInt32(reader("user_id"))
                             fullname = reader("fullname").ToString()
                         Else
                             MessageBox.Show("Username tidak ditemukan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -104,7 +80,7 @@ Public Class ChangePassword
                 End Using
 
                 ' Update password
-                Dim updateQuery As String = "UPDATE users SET password = @password, updated_at = NOW() WHERE id = @userId"
+                Dim updateQuery As String = "UPDATE users SET password = @password, updated_at = NOW() WHERE user_id = @userId"
                 Using updateCmd As New MySqlCommand(updateQuery, conn)
                     updateCmd.Parameters.AddWithValue("@password", TextBox2.Text)
                     updateCmd.Parameters.AddWithValue("@userId", userId)
@@ -112,23 +88,29 @@ Public Class ChangePassword
                     Dim result As Integer = updateCmd.ExecuteNonQuery()
 
                     If result > 0 Then
+                        isSuccess = True ' Set flag
+                        
                         ' Log activity
                         Try
                             Dim logQuery As String = "INSERT INTO activity_logs (user_id, aksi, detail, waktu) VALUES (@userId, 'Change Password', @detail, NOW())"
                             Using logCmd As New MySqlCommand(logQuery, conn)
                                 logCmd.Parameters.AddWithValue("@userId", userId)
-                                logCmd.Parameters.AddWithValue("@detail", $"User {TextBox1.Text} berhasil mengganti password")
+                                logCmd.Parameters.AddWithValue("@detail", "User " & TextBox1.Text & " berhasil mengganti password")
                                 logCmd.ExecuteNonQuery()
                             End Using
                         Catch
                             ' Silent fail for activity log
                         End Try
 
-                        MessageBox.Show($"Password berhasil diubah untuk {fullname}!" & vbCrLf & "Silakan login dengan password baru.", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        MessageBox.Show("Password berhasil diubah untuk " & fullname & "!" & vbCrLf & "Silakan login dengan password baru.", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
                         ' Kembali ke login form
-                        Dim loginForm As New LoginForm()
-                        loginForm.Show()
+                        If ParentLogin IsNot Nothing Then
+                            ParentLogin.Show()
+                        Else
+                            Dim loginForm As New LoginForm()
+                            loginForm.Show()
+                        End If
                         Me.Close()
                     Else
                         MessageBox.Show("Gagal mengubah password! Silakan coba lagi.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -137,30 +119,38 @@ Public Class ChangePassword
             End Using
 
         Catch ex As MySqlException
-            MessageBox.Show($"Error database: {ex.Message}", "Error Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Error database: " & ex.Message, "Error Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Catch ex As Exception
-            MessageBox.Show($"Terjadi kesalahan: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Terjadi kesalahan: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         ' Kembali ke login form
-        If ParentLogin IsNot Nothing Then
-            ParentLogin.Show()
-        Else
-            Dim loginForm As New LoginForm()
-            loginForm.Show()
-        End If
-        Me.Close()
+        Me.Close() ' FormClosing event will handle showing the parent
     End Sub
 
     Private Sub ChangePassword_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        ' Kembali ke login form
-        If ParentLogin IsNot Nothing Then
+        ' Show parent login form unless registration was a success
+        If Not isSuccess AndAlso ParentLogin IsNot Nothing Then
             ParentLogin.Show()
-        Else
-            Dim loginForm As New LoginForm()
-            loginForm.Show()
         End If
+    End Sub
+    
+    ' --- Hover Handlers ---
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs)
+        Button1.BackColor = lightNavyColor
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs)
+        Button1.BackColor = navyColor
+    End Sub
+
+    Private Sub Button2_MouseEnter(sender As Object, e As EventArgs)
+        Button2.BackColor = lightGreyHoverColor
+    End Sub
+
+    Private Sub Button2_MouseLeave(sender As Object, e As EventArgs)
+        Button2.BackColor = whiteColor
     End Sub
 End Class
